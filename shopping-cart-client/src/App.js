924ac9df05e81745fcbec3b9cc2aa7371cdd3542
Khalid Eltawagny <khalid.eltawagny@gmail.com>
2024-03-29 15:05:41 +0200

    Added initial file structure

---
+++ shopping-cart-client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p1>This is the app that I am gonna develop</p1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
