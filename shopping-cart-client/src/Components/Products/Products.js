0e62042d23aaf746dd50566df725b06cb11d4ab1
Khalid Eltawagny <khalid.eltawagny@gmail.com>
2024-04-09 12:04:46 +0200

    Added product modal

--- shopping-cart-client/src/Components/Products/Products.js
+++ shopping-cart-client/src/Components/Products/Products.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductModal from "./ProductModal";
 
 import "../../css/Products/Products.css";
 function Products(props) {
+  const [product, setProduct] = useState("");
+
+  const openModal = (product) => {
+    setProduct(product);
+  };
+
+  const closeModal = () => {
+    setProduct(false);
+  };
+
   return (
     <div className="products-wrapper">
       {props.products.map((product) => (
         <div className="product-item" key={product.id}>
-          <img src={product.imageUrl} alt={product.title} />
+          <a href="#" onClick={() => openModal(product)}>
+            <img src={product.imageUrl} alt={product.title} />
+          </a>
           <div className="product-desc">
             <p>{product.title}</p>
             <span>{product.price}</span>
@@ -14,6 +27,7 @@ function Products(props) {
           <button>Add To Cart</button>
         </div>
       ))}
+      <ProductModal product={product} closeModal={closeModal} />
     </div>
   );
 }
