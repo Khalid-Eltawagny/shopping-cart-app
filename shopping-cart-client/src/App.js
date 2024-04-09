ad8ea600dffaec2fa8313814486016e4b6376545
Khalid Eltawagny <khalid.eltawagny@gmail.com>
2024-04-09 17:05:12 +0200

    Added filter component

--- shopping-cart-client/src/App.js
+++ shopping-cart-client/src/App.js
@@ -4,16 +4,50 @@ import Footer from "./Components/Footer/Footer";
 import Products from "./Components/Products/Products";
 import data from "./data.json";
 import { useState } from "react";
+import Filter from "./Components/Filter/Filter";
 
 function App() {
   const [products, setProducts] = useState(data);
+
+  const allProducts = data;
+
+  const handleFilterBySize = (e) => {
+    if (e.target.value == "ALL") {
+      setProducts(data);
+    } else {
+      let productsClone = [...allProducts];
+      let newProducts = productsClone.filter(
+        (p) => p.sizes.indexOf(e.target.value) != -1
+      );
+      setProducts(newProducts);
+    }
+  };
+
+  const handleFilterByOrder = (e) => {
+    let order = e.target.value;
+    let productsClone = [...products];
+    let newProducts = productsClone.sort(function (a, b) {
+      if (order == "lowest") {
+        return a.price - b.price;
+      } else if (order == "highest") {
+        return b.price - a.price;
+      } else {
+        return a.id < b.id ? 1 : -1;
+      }
+    });
+    setProducts(newProducts);
+  };
+
   return (
     <div className="layout">
       <Header />
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <div className="filter-wrapper"> Filter </div>
+          <Filter
+            handleFilterByOrder={handleFilterByOrder}
+            handleFilterBySize={handleFilterBySize}
+          />
         </div>
       </main>
       <Footer />
---
+++ shopping-cart-client/src/Components/Filter/Filter.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../css/Filter/Filter.css";
+
+export default function Filter(props) {
+  return (
+    <div className="filter-wrapper">
+      <h2 className="filter-title"> Filter </h2>
+      <div className="num-of-products"> Number of Products 4 </div>
+      <div className="filter-by-size">
+        <span>Filter</span>
+        <select className="filter-select" onChange={props.handleFilterBySize}>
+          <option value="ALL">ALL</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
+          <option value="XL">XL</option>
+          <option value="XXL">XXL</option>
+        </select>
+      </div>
+      <div className="filter-by-size">
+        <span>Order</span>
+        <select className="filter-select" onChange={props.handleFilterByOrder}>
+          <option value="latest">Latest</option>
+          <option value="lowest">Lower</option>
+          <option value="highest">Highest</option>
+        </select>
+      </div>
+    </div>
+  );
+}
